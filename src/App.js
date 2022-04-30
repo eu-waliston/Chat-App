f8f0d0f43c0528682eb9bc87d51661a781155c68
bkt-tonton <goneisgone@hotmail.com>
2022-04-30 17:52:51 -0300

    Fina project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+
 import './App.css';
 
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
 function App() {
+
+  if(!localStorage.getItem('username')) {
+    return <LoginForm />
+  }  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine 
+      height="100vh"
+      projectID="c2cec35b-2a06-4ee9-a9a8-e08ce0444206"
+      userName={localStorage.getItem('ussername')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={ (chatAppPrps) => <ChatFeed {...chatAppPrps}/> }
+    />
   );
 }
 
